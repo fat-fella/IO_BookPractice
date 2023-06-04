b3a760fe0b5aee69775e4f07afb2c30ad2bc26ab
fat-fella <aznclee711@gmail.com>
2023-06-04 23:32:32 +0900

    IO_BookPractice
    3번까지

---
+++ IO_BookPractice/src/stream/inputstream/SystemInTest1.java
@@ -0,0 +1,19 @@
+package stream.inputstream;
+
+import java.io.IOException;
+
+public class SystemInTest1 {
+	public static void main(String[] args) throws IOException{
+		System.out.println("알파벳을 하나 쓰고 [Enter]를 누르세요");
+		
+		int i;
+		try {
+			i = System.in.read();
+			System.out.println(i);
+			System.out.println((char)i);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
